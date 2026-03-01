5647297e1ad6ec7fc2775b3bbb3690ad44bee836
descensionconspiracy <151304475+descensionconspiracy@users.noreply.github.com>
2026-02-28 22:37:53 -0300

    Add files via upload

--- achecopy/app/layout.tsx
+++ achecopy/app/layout.tsx
@@ -3,8 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'ACHERON',
-  description: '',
-  generator: 'v0.dev',
+  description: 'a character sheet for ACHERON, from HONKAI: STAR RAIL.',
 }
 
 export default function RootLayout({
@@ -14,6 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://www.bosenmori.lat/" />
+            <meta property="twitter:title" content="ACHERON" />
+            <meta property="twitter:description" content="a character sheet for ACHERON, from HONKAI: STAR RAIL." />
+            <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" /></head>
       <body>{children}</body>
     </html>
   )
